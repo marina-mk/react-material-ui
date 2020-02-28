3897f93bb451aa106a966050abbfab47205656b4
Maryna Manzhula <maryna.manzhula@gmail.com>
2020-02-28 12:49:07 +0300

    feature: add redux store

--- src/index.js
+++ src/index.js
@@ -19,6 +19,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from 'store';
 
 // core components
 import Admin from "layouts/Admin.js";
@@ -28,11 +30,13 @@ import "assets/css/material-dashboard-react.css?v=1.8.0";
 const hist = createBrowserHistory();
 
 ReactDOM.render(
-  <Router history={hist}>
-    <Switch>
-      <Route path="/admin" component={Admin} />
-      <Redirect from="/" to="/admin/dashboard" />
-    </Switch>
-  </Router>,
+  <Provider store={store}>
+    <Router history={hist}>
+      <Switch>
+        <Route path="/admin" component={Admin} />
+        <Redirect from="/" to="/admin/dashboard" />
+      </Switch>
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
